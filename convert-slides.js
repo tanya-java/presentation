9ad5ec048cc840c78f8143ca71a194405c80f507
Dmitry Rakovets <dmitryrakovets@gmail.com>
2020-06-13 13:32:55 +0300

    env: add converter for slides

---
+++ convert-slides.js
@@ -0,0 +1,16 @@
+// Load Asciidoctor.js and the reveal.js converter
+var asciidoctor = require('@asciidoctor/core')()
+var asciidoctorRevealjs = require('@asciidoctor/reveal.js')
+asciidoctorRevealjs.register()
+
+// Convert the document 'presentation.adoc' using the reveal.js converter
+var options = {
+    safe: 'safe',
+    backend: 'revealjs',
+    attributes: [
+        'revealjsdir=/node_modules/reveal.js@',
+        'revealjs_theme=white',
+        'source-highlighter=highlightjs',
+        'highlightjs-languages: xml, java, json, html, css'
+    ]
+}
